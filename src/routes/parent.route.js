81faab305815962b20ef1d5231b7922b15d3c78c
Yashas S U <yashasreddy2007@gmail.com>
2026-04-16 10:49:54 +0530

    Delete src directory

--- src/routes/parent.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router();
-const parentController=require("../controllers/parent.controllers")
-// router.post("/link",parentController.postParent)
-// router.get("/children",parentController.getParent)
-// router.get("/:studentId",parentController.getParent)
-// router.put("/permissions",parentController.updateParent)
-module.exports=router;
--- src/routes/profile.route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router();
-const profileController = require("../controllers/profile.controllers")
-const authMiddleware = require("../middleware/auth.middle")
-router.get("/getme",authMiddleware,profileController.getProfile)  //done
-router.put("/update",authMiddleware,profileController.updateProfile) //done
-
-module.exports=router;
